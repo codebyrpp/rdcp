01436fb9d127b6406463a53b43e19d8e2c5031ad
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-08-22 23:15:41 +0530

    refactor toast component to include success variant

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -30,6 +30,8 @@ const toastVariants = cva(
         default: "border bg-background text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
+        success:
+          "border bg-background text-foreground border-t-green-500 border-t-4"
       },
     },
     defaultVariants: {
