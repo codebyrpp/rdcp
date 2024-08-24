4b74e03127b57c0562ea4e999648eb700871020d
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-08-24 23:12:30 +0530

    refactor: Update Preview component styling

--- src/components/builder/components/PreviewDialogBtn.tsx
+++ src/components/builder/components/PreviewDialogBtn.tsx
@@ -4,8 +4,8 @@ import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { FormElements } from "./FormElements";
 
 function PreviewDialogBtn() {
-    const {elements} = useDesigner();
-    
+    const { elements } = useDesigner();
+
     return (
         <Dialog>
             <DialogTrigger asChild>
@@ -13,21 +13,21 @@ function PreviewDialogBtn() {
                     Preview
                 </Button>
             </DialogTrigger>
-            <DialogContent  className="w-2/3 h-screen max-h-screen max-w-full flex flex-col flex-grow p-0 gap-0">
-                <div className="px-4 -y-2 border-b">
-                    <p className="text-lg font-bold"> Form Preview</p>
-                    <p className="text-sm text-muted-foreground">This is how your form will look like to your users.</p>
-                </div>
-                <div className="flex-grow flex items-center justify-center p-8 overflow-y-auto">
-                    <div className="flex flex-col gap-4 flex-grow bg-gray-400 h-full w-full rounded-2xl p-8 overflow-y-auto">
+            
+            {/* The Preview */}
+            <DialogContent className="w-[80vw] max-w-full flex flex-col h-[90vh] bg-slate-200">
+                <div className="flex-1 max-h-full h-full flex-grow flex items-center justify-center overflow-y-auto">
+                    <div className="flex flex-col gap-1 flex-grow
+                        bg-slate-200 rounded-md px-24 pt-5 
+                        h-full w-full overflow-y-auto">
                         {elements.map((element) => {
                             const FormComponent = FormElements[element.type].formComponent;
                             return (
-                                <div key={element.id} className="bg-gray-200 p-4 rounded-md">
-                                <FormComponent elementInstance={element} />
+                                <div key={element.id} className="bg-white p-4 rounded-md">
+                                    <FormComponent elementInstance={element} />
                                 </div>
                             );
-                    })}
+                        })}
                     </div>
                 </div>
             </DialogContent>
