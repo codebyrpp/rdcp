dafe1286de913a1c370db5b3131b0ae5b68251b8
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-09-07 23:54:37 +0530

    Refactor idGenerator to use UUIDv4 for generating unique IDs

--- src/components/builder/idGenerator.ts
+++ src/components/builder/idGenerator.ts
@@ -1,3 +1,9 @@
-export function idGenerator():string {
-    return Math.floor(Math.random()* 10001).toString()
+export function idGenerator(): string {
+    return uuidv4();
+}
+
+function uuidv4() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+        (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+    );
 }
