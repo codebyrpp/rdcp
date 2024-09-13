fd43695cd251882eec8eec0ef086943fc5f80d37
disumi <disumipathi01@gmail.com>
2024-09-13 19:26:50 +0530

    added collaborators api slice

---
+++ src/state/apiSlices/collaboratorsApi.ts
@@ -0,0 +1,59 @@
+import { apiSlice } from "../api";
+import { ProjectRole } from "@/models/projects";
+
+interface AddCollaboratorDTO {
+  id: string;
+  email: string;
+  roles: ProjectRole[];
+  message: string;
+}
+
+interface InviteCollaboratorsRequest {
+  projectId: string;
+  emails: string[];
+  roles: ProjectRole[];
+}
+
+interface UpdateCollaboratorRolesRequest {
+  projectId: string;
+  collaboratorId: string;
+  roles: ProjectRole[];
+}
+
+interface RemoveCollaboratorRequest {
+  projectId: string;
+  collaboratorId: string;
+}
+
+export const inviteMembersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    inviteCollaborators: builder.mutation<AddCollaboratorDTO[], InviteCollaboratorsRequest>({
+      query: ({ projectId, emails, roles }) => ({
+        url: `projects/${projectId}/invite`, 
+        method: 'POST',
+        body: { emails, roles }, 
+      }),
+    }),
+
+    updateCollaboratorRoles: builder.mutation<AddCollaboratorDTO, UpdateCollaboratorRolesRequest>({
+      query: ({ projectId, collaboratorId, roles }) => ({
+        url: `projects/${projectId}/collaborators/${collaboratorId}/roles`,
+        method: 'PATCH',
+        body: { roles }, 
+      }),
+    }),
+
+    removeCollaborator: builder.mutation<{ message: string }, RemoveCollaboratorRequest>({
+      query: ({ projectId, collaboratorId }) => ({
+        url: `projects/${projectId}/collaborators/${collaboratorId}`,
+        method: 'DELETE',
+      }),
+    }),
+  }),
+});
+
+export const {
+  useInviteCollaboratorsMutation,
+  useUpdateCollaboratorRolesMutation,
+  useRemoveCollaboratorMutation,
+} = inviteMembersApiSlice;
