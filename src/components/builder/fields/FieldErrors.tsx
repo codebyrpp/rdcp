8630f56a9939c417bcdc0482b3a2e6f48fd5cf59
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-09-30 16:02:00 +0530

    Refactor FieldErrors component to add key prop to error divs

--- src/components/builder/fields/FieldErrors.tsx
+++ src/components/builder/fields/FieldErrors.tsx
@@ -3,7 +3,7 @@ import { FaExclamationCircle } from "react-icons/fa";
 export const FieldErrors = ({ errors }: { errors: string[] }) => {
     return <>
         {errors.map((error, index) => (
-            <div className="text-red-500 text-xs flex">
+            <div key={index} className="text-red-500 text-xs flex">
                 <FaExclamationCircle className="mr-2" />
                 <div>{error}</div>
             </div>
