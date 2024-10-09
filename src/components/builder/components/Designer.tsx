a770bd0d57706a527debc991fac5dfcbb74b47de
Praveenasarma Baskarakurukkal <baskarakurukkal.21@cse.mrt.ac.lk>
2024-10-09 23:37:19 +0530

    Tooltip
    (cherry picked from commit d212e17e3d707b6bd6abb18a514cacda9f40a6b5)

--- src/components/builder/components/Designer.tsx
+++ src/components/builder/components/Designer.tsx
@@ -1,14 +1,24 @@
 "use client";
 
-import { DragEndEvent, useDndMonitor, useDraggable, useDroppable } from "@dnd-kit/core";
+import {
+  DragEndEvent,
+  useDndMonitor,
+  useDraggable,
+  useDroppable,
+} from "@dnd-kit/core";
 import { cn } from "@/lib/utils";
-import { ElementsType, FormElementInstance, FormElements } from "./FormElements";
+import {
+  ElementsType,
+  FormElementInstance,
+  FormElements,
+} from "./FormElements";
 import { idGenerator } from "../idGenerator";
 import useDesigner from "../hooks/useDesigner";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { BiSolidTrash } from "react-icons/bi";
 import { DesignerFormElementsPanel, DesignerPropertiesPanel } from "./DesignerSidebar";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 
 function Designer({ form }: {
   form: {
@@ -31,13 +41,17 @@ function Designer({ form }: {
       if (!active || !over) return;
 
       const isDesignerBtnElement = active.data?.current?.isDesignerBtnElement;
-      const isDroppingOverDesignerDropArea = over.data?.current?.isDesignerDropArea;
+      const isDroppingOverDesignerDropArea =
+        over.data?.current?.isDesignerDropArea;
 
-      const droppingSidebarBtnOverDesignerDropArea = isDesignerBtnElement && isDroppingOverDesignerDropArea;
+      const droppingSidebarBtnOverDesignerDropArea =
+        isDesignerBtnElement && isDroppingOverDesignerDropArea;
 
       if (droppingSidebarBtnOverDesignerDropArea) {
         const type = active.data?.current?.type;
-        const newElement = FormElements[type as ElementsType].construct(idGenerator());
+        const newElement = FormElements[type as ElementsType].construct(
+          idGenerator()
+        );
 
         addElement(elements.length, newElement);
         return;
@@ -51,7 +65,9 @@ function Designer({ form }: {
 
       if (droppingSidebarBtnOverDesignerElement) {
         const type = active.data?.current?.type;
-        const newElement = FormElements[type as ElementsType].construct(idGenerator());
+        const newElement = FormElements[type as ElementsType].construct(
+          idGenerator()
+        );
 
         const overId = over.data?.current?.elementId;
 
@@ -76,7 +92,9 @@ function Designer({ form }: {
         const activeId = active.data?.current?.elementId;
         const OverId = over.data?.current?.elementId;
 
-        const activeElementIndex = elements.findIndex((el) => el.id === activeId);
+        const activeElementIndex = elements.findIndex(
+          (el) => el.id === activeId
+        );
         const overElementIndex = elements.findIndex((el) => el.id === OverId);
 
         if (activeElementIndex === -1 || overElementIndex === -1) {
@@ -98,10 +116,13 @@ function Designer({ form }: {
   return (
     <div className="flex w-screen overflow-hidden h-full relative">
       <DesignerFormElementsPanel />
-      <div className="flex flex-col p-4 gap-1 w-screen overflow-y-auto" >
-        <div className="flex justify-center" style={{
-          width: "calc(100vw - 400px)",
-        }}>
+      <div className="flex flex-col p-4 gap-1 w-screen overflow-y-auto">
+        <div
+          className="flex justify-center"
+          style={{
+            width: "calc(100vw - 400px)",
+          }}
+        >
           <div className="max-w-[800px] flex-1 px-4">
             <div className="w-full bg-background p-4 rounded-md border-t-slate-500 border-t-[6px]">
               <p className="text-xl font-bold">{form.name}</p>
@@ -109,9 +130,10 @@ function Designer({ form }: {
             </div>
           </div>
         </div>
-        <div style={{
-          width: "calc(100vw - 400px)",
-        }}
+        <div
+          style={{
+            width: "calc(100vw - 400px)",
+          }}
           className="h-full flex-1"
           onClick={() => {
             if (selectedElement) setSelectedElement(null);
@@ -124,9 +146,11 @@ function Designer({ form }: {
               // Initial Drop zone when no element over it
               <div className="flex flex-col justify-center h-full flex-grow items-center">
                 <p className="text-muted-foreground mt-2 text-md text-center">
-                  Drag and drop elements from the <br /> left sidebar to create your form
+                  Drag and drop elements from the <br /> left sidebar to create
+                  your form
                 </p>
-              </div>)}
+              </div>
+            )}
             {droppable.isOver && elements.length === 0 && (
               // Initial Drop zone when an element is over it
               <DropZoneHint />
@@ -145,12 +169,14 @@ function Designer({ form }: {
         <DesignerPropertiesPanel />
       </div>
     </div>
-  )
+  );
 }
 
-const DropZoneHint = () => (<div className="p-4 w-full">
-  <div className="h-[120px] rounded-md bg-slate-300"></div>
-</div>);
+const DropZoneHint = () => (
+  <div className="p-4 w-full">
+    <div className="h-[120px] rounded-md bg-slate-300"></div>
+  </div>
+);
 
 function DesignerElementWrapper({ element }: { element: FormElementInstance }) {
   const { removeElement, setSelectedElement, selectedElement } = useDesigner();
@@ -192,8 +218,7 @@ function DesignerElementWrapper({ element }: { element: FormElementInstance }) {
       {...draggable.listeners}
       {...draggable.attributes}
       className={cn(`relative flex flex-col bg-white
-        hover:cursor-pointer rounded-md ring-1 ring-accent ring-inset border border-slate-300`,
-      )}
+        hover:cursor-pointer rounded-md ring-1 ring-accent ring-inset border border-slate-300`)}
       onMouseEnter={() => {
         setMouseIsOver(true);
       }}
@@ -210,15 +235,23 @@ function DesignerElementWrapper({ element }: { element: FormElementInstance }) {
       {mouseIsOver && (
         <>
           <div className="absolute right-0 h-full group">
-            <Button className="flex justify-center h-full border rounded-md rounded-l-none 
-            bg-red-400 group-hover:bg-red-500"
-              variant={"outline"}
-              onClick={(e) => {
-                e.stopPropagation();
-                removeElement(element.id);
-              }}>
-              <BiSolidTrash className="h-8 w-6" />
-            </Button>
+            <Tooltip>
+              <TooltipTrigger>
+                <Button
+                  className="flex justify-center h-full border rounded-md rounded-l-none bg-red-400 group-hover:bg-red-500"
+                  variant={"outline"}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    removeElement(element.id);
+                  }}
+                >
+                  <BiSolidTrash className="h-8 w-6" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Remove this element</p>
+              </TooltipContent>
+            </Tooltip>
           </div>
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
             <p className="text-sm">Click for properties or drag to move</p>
@@ -231,10 +264,14 @@ function DesignerElementWrapper({ element }: { element: FormElementInstance }) {
       {bottomHalf.isOver && (
         <div className="absolute bottom-0 w-full rounded-md h-[7px] bg-primary rounded-t-none" />
       )}
-      <div className={cn("flex w-full items-center rounded-md bg-background p-4 pointer-events-none opacity-100",
-        mouseIsOver && "opacity-30",
-        selectedElement === element && "border-l-4 border-l-slate-500 elevation-2 shadow-md",
-      )}>
+      <div
+        className={cn(
+          "flex w-full items-center rounded-md bg-background p-4 pointer-events-none opacity-100",
+          mouseIsOver && "opacity-30",
+          selectedElement === element &&
+            "border-l-4 border-l-slate-500 elevation-2 shadow-md"
+        )}
+      >
         <DesignerElement elementInstance={element} />
       </div>
     </div>
--- src/components/feats/forms/CreateForm.tsx
+++ src/components/feats/forms/CreateForm.tsx
@@ -1,35 +1,55 @@
-import { dialogCloseWrapper } from '@/components/common/DialogCloseWrapper'
-import FormCreateForm from '@/components/forms/FormCreateForm'
-import { Button } from '@/components/ui/button'
-import { Dialog, DialogClose, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
-import { ReactNode } from 'react'
-import { FaPlus } from 'react-icons/fa6'
+import { dialogCloseWrapper } from "@/components/common/DialogCloseWrapper";
+import FormCreateForm from "@/components/forms/FormCreateForm";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { ReactNode } from "react";
+import { FaPlus } from "react-icons/fa6";
 
 const CreateForm = () => {
-    return (
-        <Dialog>
+  return (
+    <Dialog>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
             <DialogTrigger asChild>
-                <Button className='flex gap-2'>New Form
-                    <FaPlus className='text-lg' />
-                </Button>
+              <Button className="flex gap-2">
+                New Form
+                <FaPlus className="text-lg" />
+              </Button>
             </DialogTrigger>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle asChild>
-                        <h4 className="text-xl font-bold">
-                            Create a New Form
-                        </h4>
-                    </DialogTitle>
-                    <DialogDescription className="hidden">
-                        Enter the form name and description to create a new project.
-                    </DialogDescription>
-                </DialogHeader>
-                <FormCreateForm
-                    buttonWrapper={dialogCloseWrapper}
-                />
-            </DialogContent>
-        </Dialog>
-    )
-}
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Create a new form</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle asChild>
+            <h4 className="text-xl font-bold">Create a New Form</h4>
+          </DialogTitle>
+          <DialogDescription className="hidden">
+            Enter the form name and description to create a new project.
+          </DialogDescription>
+        </DialogHeader>
+        <FormCreateForm buttonWrapper={dialogCloseWrapper} />
+      </DialogContent>
+    </Dialog>
+  );
+};
 
-export default CreateForm
+export default CreateForm;
