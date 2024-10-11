ab9cb5b3b7bdb66601a27450255be25230161992
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-10-11 23:54:32 +0530

    Refactor password input component to use reusable PasswordInput component

--- src/components/forms/FormLogin.tsx
+++ src/components/forms/FormLogin.tsx
@@ -14,6 +14,7 @@ import FormWrapper from "@/components/forms/FormWrapper"
 import { useLoginFormViewModel } from "@/viewmodels/login"
 import { useNavigate } from "react-router-dom"
 import { FORGOT_PASSWORD_ROUTE } from "@/constants/routes"
+import PasswordInput from "../common/PasswordInput"
 
 
 export default function LoginForm() {
@@ -49,7 +50,7 @@ export default function LoginForm() {
               <FormItem>
                 <FormLabel>Password</FormLabel>
                 <FormControl>
-                  <Input placeholder="********" type="password" {...field} />
+                  <PasswordInput {...field} />
                 </FormControl>
                 <FormDescription>
 
